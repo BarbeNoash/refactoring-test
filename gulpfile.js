de240816a2be7b459cf70fa128f5f9e5a28acd80
Anoureth <anoureth.pongrattana@gmail.com>
2017-12-10 16:10:21 +0100

    Fix : gulp architecture - add module to use sass preprocessing style - add module to reduce image size and add asset file - clean duplicate code - modified task flow and watchers for best code reading

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,71 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-var nodemon = require('gulp-nodemon');
-var notify = require('gulp-notify');
-var clean = require('gulp-clean');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const nodemon = require('gulp-nodemon');
+const notify = require('gulp-notify');
+const clean = require('gulp-clean');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
 
-gulp.task('es6', () => {
-    return gulp.src(["app.js", "routes/*", 'lib/*', 'middleware/*'], {base: "."})
-        .pipe(babel({presets: ['es2015']}))
-        .pipe(gulp.dest('dist'))
+// Transpile with babel and set in public folder
+gulp.task('app', () => {
+    return gulp.src(['app.js'], { base: "." })
+        .pipe(babel({ presets: ['es2015'] }))
+        .pipe(gulp.dest('public/dist/'))
 });
 
-var notify = require('gulp-notify');
-var clean = require('gulp-clean');
-
-gulp.task('json', () => {
-    return gulp.src(['config/*'], {base: "."})
-        .pipe(gulp.dest('dist'))
+// Transpile folder with babel
+gulp.task('run', () => {
+    return gulp.src(['routes/*', 'controllers/*', 'libs/**/*'], { base: "." })
+        .pipe(babel({ presets: ['es2015'] }))
 });
 
-gulp.task('clean', function () {
-    return gulp.src('dist/', {read: false})
+// Use sass preprocecing for styles
+gulp.task('sass', () => {
+    return gulp.src(['sass/**/*'], { base: "." })
+        .pipe(notify({ message: 'preprocecing sass into styles.css' }))
+        .pipe(sass().on('error', sass.logError))
+        // Concat to output instead of directory
+        .pipe(concat('styles.css'))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('public/dist/'))
+});
+
+// Optimize size of images
+gulp.task('images', function () {
+    return gulp.src(['assets/images/*'])
+        .pipe(notify({ message: 'images built' }))
+        .pipe(imagemin())
+        .pipe(gulp.dest('public/images/'))
+});
+
+// Build fonts
+gulp.task('fonts', function () {
+    return gulp.src(['assets/fonts/*'])
+        .pipe(notify({ message: 'built fonts' }))
+        .pipe(gulp.dest('public/fonts/'));
+});
+
+// Clean public directory
+gulp.task('clean', () => {
+    return gulp.src('public/', { read: false })
         .pipe(clean());
 });
 
-
-gulp.task('server', function() {
+// Lauch localhost server
+gulp.task('server', () => {
     nodemon({
-        script: 'dist/app.js',
-        watch: ["app.js", "routes/*", 'lib/*', 'middleware/*', 'config/*'],
-        ext: 'js json',
-        tasks: ['build']
-    }).on('restart', function() {
-        gulp.src('app.js').pipe(notify('Server successfully restarted'));
+        script: 'public/dist/app.js',
+        watch: ['app.js', 'routes/*', 'controllers/*', 'libs/**/*', 'sass/**/*'],
+        ext: 'js json scss png gif jpg svg',
+        tasks: ['build'],
+    }).on('restart', () => {
+        gulp.src('public/dist/app.js').pipe(notify('Server successfully restarted'));
     })
 });
 
+// Define task lauch config
 gulp.task('default', ['build', 'server']);
-gulp.task('build', ['es6', 'json']);
+gulp.task('build', ['app', 'run', 'sass', 'images', 'fonts']);
 gulp.task('heroku', ['clean', 'build']);
